2bcb34dc8a6c62d36746c4e6673b7fa3ec966a8c
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-28 12:06:32 +0200

    bsq.h

--- bsq.h
+++ bsq.h
@@ -6,19 +6,21 @@
 /*   By: rhunders <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 11:45:37 by rhunders          #+#    #+#             */
-/*   Updated: 2018/08/28 11:53:27 by rhunders         ###   ########.fr       */
+/*   Updated: 2018/08/28 12:06:22 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BSQ_H
 # define BSQ_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 typedef struct s_obs
 {
 	int x;
 	struct s_obs *next;
 }				t_obs;
-
 typedef struct	s_map
 {
 	int xs;
@@ -28,5 +30,4 @@ typedef struct	s_map
 	char *map;
 	t_obs **obs;
 }				t_map;
-
 #endif
