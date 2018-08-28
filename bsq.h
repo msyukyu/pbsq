dbcebc366dfc3cc5a1e9f62ca1867853c0ed7224
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-28 12:34:53 +0200

    norm

--- bsq.h
+++ bsq.h
@@ -6,28 +6,37 @@
 /*   By: rhunders <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/28 11:45:37 by rhunders          #+#    #+#             */
-/*   Updated: 2018/08/28 12:06:22 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/28 12:34:44 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BSQ_H
 # define BSQ_H
+# define BUFFER_SIZE 4096
+# define MAP_SIZE (map.ye + 1)
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 
-typedef struct s_obs
+typedef struct		s_square
 {
-	int x;
-	struct s_obs *next;
-}				t_obs;
-typedef struct	s_map
+	int				xs;
+	int				ys;
+	int				xe;
+	int				ye;
+}
+typedef struct		s_obs
 {
-	int xs;
-	int ys;
-	int xe;
-	int ye;
-	char *map;
-	t_obs **obs;
-}				t_map;
+	int				x;
+	struct s_obs	*next;
+}					t_obs;
+typedef struct		s_map
+{
+	int				xs;
+	int				ys;
+	int				xe;
+	int				ye;
+	char			*map;
+	t_obs			**obs;
+}					t_map;
 #endif
